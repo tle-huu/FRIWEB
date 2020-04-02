6562ec716eb8b9135928a6fccf72ca7e42eb26ba
Terence Le Huu Phuong <tlehuu@hotmail.fr>
2020-04-02 11:33:37 +0200

    Adding filtering skeleton

---
+++ filtering/filtering.py
@@ -0,0 +1,15 @@
+import nltk
+import re
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+def filter_corpus(tokenized_corpus, stop_words):
+
+	return tokenized_corpus
+
+
+def lemmatize_corpus(tokenized_corpus):
+
+	return tokenized_corpus
